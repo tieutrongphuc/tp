7d25e09dd0ddb967a664895f4986a79a14e1d156
Duyy0406 <baoduytran01042006@gmail.com>
2025-10-12 00:44:05 +0800

    Update Phone validation

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -11,8 +11,8 @@ public class Phone {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
-    public static final String VALIDATION_REGEX = "\\d{3,}";
+            "Phone numbers should only contain numbers and the symbol +, and it should be at least 3 digits long";
+    public static final String VALIDATION_REGEX = "^\\+?\\d{3,}$";
     public final String value;
 
     /**
@@ -22,8 +22,9 @@ public class Phone {
      */
     public Phone(String phone) {
         requireNonNull(phone);
-        checkArgument(isValidPhone(phone), MESSAGE_CONSTRAINTS);
-        value = phone;
+        String trimmedPhone = phone.trim();
+        checkArgument(isValidPhone(trimmedPhone), MESSAGE_CONSTRAINTS);
+        value = trimmedPhone.replaceAll("\\s+", "");
     }
 
     /**
