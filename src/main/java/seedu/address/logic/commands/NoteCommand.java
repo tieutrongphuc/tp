5b31e2675d2b3a7b83e5b441078396a79522e9b1
0ribta1 <junxiang5563@gmail.com>
2025-10-13 21:44:10 +0800

    Resolve Checkstyle violations

--- src/main/java/seedu/address/logic/commands/NoteCommand.java
+++ src/main/java/seedu/address/logic/commands/NoteCommand.java
@@ -4,10 +4,9 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.List;
 
-
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Note;
 import seedu.address.model.person.Person;
