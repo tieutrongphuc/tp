a40c3eb8eaa0f427c173f2f6d7455f3eb825742b
0ribta1 <junxiang5563@gmail.com>
2025-10-13 21:22:26 +0800

    Resolved missing symbols Index and Note

--- src/main/java/seedu/address/logic/commands/NoteCommand.java
+++ src/main/java/seedu/address/logic/commands/NoteCommand.java
@@ -2,13 +2,16 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
+import seedu.address.model.person.Note;
+import seedu.address.model.person.Person;
 
 public class NoteCommand extends Command {
 
     public static final String COMMAND_WORD = "note";
 
-    public static final String MESSAGE_SUCCESS = "Displayed notes";
+    public static final String MESSAGE_SUCCESS = "Added note";
 
     private final Index index;
     private final Note note;
@@ -17,7 +20,6 @@ public class NoteCommand extends Command {
         this.index = index;
         this.note = note;
     }
-
 
     @Override
     public CommandResult execute(Model model) {
