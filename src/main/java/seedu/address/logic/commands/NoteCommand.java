33eeb3c4d77eb67e3c653d0142d67253301e567c
0ribta1 <junxiang5563@gmail.com>
2025-10-13 21:16:01 +0800

    Resolve trailing whitespace failure

--- src/main/java/seedu/address/logic/commands/NoteCommand.java
+++ src/main/java/seedu/address/logic/commands/NoteCommand.java
@@ -6,9 +6,18 @@ import seedu.address.model.Model;
 
 public class NoteCommand extends Command {
 
-    public static final String COMMAND_WORD = "notes";
+    public static final String COMMAND_WORD = "note";
 
     public static final String MESSAGE_SUCCESS = "Displayed notes";
+
+    private final Index index;
+    private final Note note;
+
+    public NoteCommand(Index index, Note note) {
+        this.index = index;
+        this.note = note;
+    }
+
 
     @Override
     public CommandResult execute(Model model) {
--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -36,10 +36,10 @@ public class SampleDataUtil {
                 new Address("Blk 47 Tampines Street 20, #17-35"), new Note("Met very very very very very very " +
                     "very very very very very very very very very very very very very very very very very very " +
                     "very very very very very very very very very very very very very very very very very very " +
-                    "long  long ago"),
+                    "long long ago"),
                 getTagSet("classmates")),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"), new Note(""), 
+                new Address("Blk 45 Aljunied Street 85, #11-31"), new Note(""),
                 getTagSet("colleagues"))
         };
     }
