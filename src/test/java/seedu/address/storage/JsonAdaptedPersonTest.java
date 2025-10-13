f88b6bf78ccff29943c426ca4bbc3ed1c134388b
0ribta1 <junxiang5563@gmail.com>
2025-10-13 21:51:58 +0800

    Resolve Checkstyle error

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -16,7 +16,6 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
-import seedu.address.model.person.Note;
 import seedu.address.model.person.Phone;
 
 public class JsonAdaptedPersonTest {
