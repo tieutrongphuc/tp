31498c6ff8a19279e046047402bb5918b71a749c
Aryann-Sharma <e1295594@u.nus.edu>
2025-10-23 21:58:39 +0800

    Add parser logic for deleting multiple persons

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import java.util.List;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
@@ -14,12 +15,14 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteCommand
      * and returns a DeleteCommand object for execution.
+     * Duplicate indexes are removed
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
         try {
-            Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
+            List<Index> indexes = ParserUtil.parseIndexes(args);
+            List<Index> uniqueIndexes = indexes.stream().distinct().toList();
+            return new DeleteCommand(uniqueIndexes);
         } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
