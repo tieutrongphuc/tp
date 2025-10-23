ea08d86495ae0d1631fdafb77dcf34f8e983d151
Duyy0406 <baoduytran01042006@gmail.com>
2025-10-23 21:55:50 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -12,20 +12,26 @@ import seedu.address.model.person.Person;
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
  * Alternatively, lists all persons in address book with any tag matching the argument keywords.
- * Keyword matching is case insensitive.
+ * Keyword matching is case-insensitive.
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-insensitive) "
-            + "and displays them as a list with index numbers.\n"
-            + "Alternatively, finds all persons whose tags match the specified tags. "
-            + "Use t/ to indicate search by tag."
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": choose one of searching by name, tag, or note.\n"
+            + "Find by name: finds all persons whose names match the specified keywords.\n"
+            + "Find by tag: finds all persons whose tags match the specified tags. "
+            + "Use 't/' to indicate search by tag.\n"
+            + "Find by note: finds all persons whose notes contain the specified keywords. "
+            + "Use 'note/' to indicate search by note.\n"
             + "\nParameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie\n"
-            + "Or: " + COMMAND_WORD + " t/friends t/colleagues";
+            + "Example: '" + COMMAND_WORD + " alice bob charlie' for find by name\n"
+            + "Or: '" + COMMAND_WORD + " t/friends t/colleagues' for find by tag\n"
+            + "Or: '" + COMMAND_WORD + " note/expressed interest in AI' for find by note\n"
+            + "Searching is case-insensitive.";
+
+    public static final String MESSAGE_MULTIPLE_SEARCH = "Please search by only one field.";
 
     private final Predicate<Person> predicate;
 
