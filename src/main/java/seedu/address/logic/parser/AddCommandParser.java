a9c04220a9b2f87780ebd33bab8394e965b585d9
Cyrus <schoolprojectcy@gmail.com>
2025-10-29 05:18:15 +0800

    Fix Checkstyle

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -6,8 +6,8 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG_TITLE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG_RESEARCH;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG_TITLE;
 
 import java.util.Set;
 import java.util.stream.Stream;
--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -7,8 +7,8 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG_TITLE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG_RESEARCH;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG_TITLE;
 import static seedu.address.logic.parser.ParserUtil.parseTags;
 
 import java.util.Collection;
