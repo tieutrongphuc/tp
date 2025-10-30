d0894818b841d49bc75ee2b8c93ccd18f7da9ca9
Aryann-Sharma <e1295594@u.nus.edu>
2025-10-30 17:25:47 +0800

    Fix Checkstyle error

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -2,8 +2,8 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.util.List;
 import java.util.LinkedHashSet;
+import java.util.List;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
