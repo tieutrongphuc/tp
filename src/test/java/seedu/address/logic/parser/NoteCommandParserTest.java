26bf7e102736c6b87a4983e6a82680c5fc51380b
0ribta1 <junxiang5563@gmail.com>
2025-10-30 21:34:33 +0800

    Resolve code quality issues

--- src/test/java/seedu/address/logic/parser/NoteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/NoteCommandParserTest.java
@@ -12,13 +12,7 @@ import org.junit.jupiter.api.Test;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.NoteCommand;
 
-/**
- * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the NoteCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the NoteCommand, and therefore we test only one of them.
- * The path variation for those two cases occur inside the ParserUtil, and
- * therefore should be covered by the ParserUtilTest.
- */
+
 public class NoteCommandParserTest {
 
     private NoteCommandParser parser = new NoteCommandParser();
