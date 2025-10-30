3c746bb904280c1ca016612b7e7ef15fed14eb63
0ribta1 <junxiang5563@gmail.com>
2025-10-30 15:16:57 +0800

    Resolve UI bugs

--- src/main/java/seedu/address/ui/ReminderCard.java
+++ src/main/java/seedu/address/ui/ReminderCard.java
@@ -33,7 +33,7 @@ public class ReminderCard extends UiPart<Region> {
         super(FXML);
         this.reminder = reminder;
         id.setText(displayedIndex + ". ");
-        personName.setText("● " + reminder.getPerson().getName().fullName);
+        personName.setText("    " + reminder.getPerson().getName().fullName);
         date.setText(reminder.getDate().toString());
         message.setText("    " + reminder.getMessage().toString());
     }
