be4179ead69360d3be829e173ee62c59ec76a032
Cyrus <schoolprojectcy@gmail.com>
2025-11-04 09:09:42 +0800

    Fix checkstyle

--- src/test/java/seedu/address/model/tag/TagTest.java
+++ src/test/java/seedu/address/model/tag/TagTest.java
@@ -1,7 +1,6 @@
 package seedu.address.model.tag;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
